7b082998af96180c419b0ee7642907a2e54a0376
Errrrrrr <zxbc2017@gmail.com>
2023-05-24 15:19:07 +0200

    Updated to press-release mechanism
    Press down key to aim, release to fire. This makes dgun work similar to smartcast skills in some of the other games.

--- cmd_instant_dgun.lua
+++ cmd_instant_dgun.lua
@@ -1,7 +1,7 @@
 function widget:GetInfo()
     return {
         name      = "Instant Dgun",
-        desc      = "Instantly fire dgun. Default keybind 'd', rebindable.",
+        desc      = "Aims when you press key down, fires when you release. Default keybind 'd', rebindable.",
         author    = "Errrrrrr",
         date      = "May 23, 2023",
         license   = "GNU GPL, v2 or later",
@@ -14,8 +14,10 @@ end
 --------------------------------------------------------------------------------
 -- Set custom_keybind_mode to false to use default 'd' keybind
 -- Custom keybind action name: instant_dgun
+-- Set retain_aiming to true if you want the dgun aim to stay up after firing
 --------------------------------------------------------------------------------
 local custom_keybind_mode = false
+local retain_aiming = false
 
 local isCommander = {}
 for unitDefID, unitDef in pairs(UnitDefs) do
@@ -24,48 +26,80 @@ for unitDefID, unitDef in pairs(UnitDefs) do
 	end
 end
 
+local commID
+
 function widget:Initialize()
     if custom_keybind_mode then
-        widgetHandler.actionHandler:AddAction(self, "instant_dgun", dgunBind, nil, "p")
+        widgetHandler.actionHandler:AddAction(self, "instant_dgun_press", showDgun, nil, "R")
+        widgetHandler.actionHandler:AddAction(self, "instant_dgun_release", fireDgun, nil, "r")
     end
 end
 
-function dgunBind(_, _, args)
+function dgunPress(_, _, args)
+    showDgun()
+end
+
+function dgunRelease(_, _, args)
     fireDgun()
 end
 
 function widget:KeyPress(key, mods, isRepeat)
-    if not custom_keybind_mode and key == 100 and not isRepeat then
-        fireDgun()
+    if not custom_keybind_mode and key == 100 and isRepeat then
+        return showDgun()   -- blocks other keypress if showDgun finds commander
     end
 end
 
+function widget:KeyRelease(key)
+    if not custom_keybind_mode and key == 100 then
+        local result = fireDgun()
+    end
+end
+
+function showDgun()
+    if commID then
+        Spring.SetActiveCommand('dgun')
+        return true
+    end
+    
+end
+
+function widget:SelectionChanged(sel)
+    
+    if #sel == 1 and isCommander[Spring.GetUnitDefID(sel[1])] then
+        commID = sel[1]
+    else
+        commID = nil
+    end
+end
+
+
 -- return true if selecting only commander and command was successful
 function fireDgun()
-    local selectedUnits = Spring.GetSelectedUnits()
-        if #selectedUnits == 1 then
-            local selectedUnitID = selectedUnits[1]
-            if isCommander[Spring.GetUnitDefID(selectedUnitID)] then
-                local mouseX, mouseY = Spring.GetMouseState()
-                local desc, args = Spring.TraceScreenRay(mouseX, mouseY, true)
-                
-                local params
-                if desc and desc == "unit" then
-                    params = {args}
-                elseif desc and desc == "ground" then
-                    params = {args[1], args[2], args[3]}
-                end
-
-                local alt, ctrl, meta, shift = GetModKeys()
-                local cmdOpts = GetCmdOpts(alt, ctrl, meta, shift, false)
-                local altOpts = GetCmdOpts(true, false, false, false, false)
-
-                Spring.GiveOrderToUnit(selectedUnitID, CMD.INSERT, {0, CMD.DGUN, cmdOpts.coded, unpack(params)}, altOpts)
-                --Spring.GiveOrderToUnit(selectedUnitID, CMD.DGUN, params, cmdOpts)
-   
-                return true
-            end
+    if commID then
+        Spring.Echo("we fired")
+        local mouseX, mouseY = Spring.GetMouseState()
+        local desc, args = Spring.TraceScreenRay(mouseX, mouseY, true)
+        
+        local params
+        if desc and desc == "unit" then
+            params = {args}
+        elseif desc and desc == "ground" then
+            params = {args[1], args[2], args[3]}
         end
+    
+        local alt, ctrl, meta, shift = GetModKeys()
+        local cmdOpts = GetCmdOpts(alt, ctrl, meta, shift, false)
+        local altOpts = GetCmdOpts(true, false, false, false, false)
+        Spring.GiveOrderToUnit(commID, CMD.INSERT, {0, CMD.DGUN, cmdOpts.coded, unpack(params)}, altOpts)
+        --Spring.GiveOrderToUnit(commID, CMD.DGUN, params, cmdOpts)
+        if not retain_aiming then
+            Spring.SetActiveCommand(0)
+        end
+        return true
+    else
+        return false
+    end
+  
 end
 
 function GetModKeys()
