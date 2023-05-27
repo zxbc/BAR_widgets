36a7f1c3dded55fb6da87fcfff0bdfa21cb7807c
Errrrrrr <zxbc2017@gmail.com>
2023-05-27 02:04:37 +0200

    Optimization

--- gui_selected_weapon_range.lua
+++ gui_selected_weapon_range.lua
@@ -243,14 +243,15 @@ function widget:DrawWorldPreUnit()
         if displayMode ~= 1 then
             glTranslate(x, y, z)
             glColor(cColor[1], cColor[2], cColor[3], cColor[4])
-            glBeginEnd(GLTRIANGLE_FAN, function()
-            local numSegments = 32
-            local angleStep = (2 * pi) / numSegments
-            for i = 0, numSegments do
-                local angle = i * angleStep
-                glVertex(sin(angle) * range, 0, cos(angle) * range)
+            local function drawCircle()
+                local numSegments = 32
+                local angleStep = (2 * pi) / numSegments
+                for i = 0, numSegments do
+                    local angle = i * angleStep
+                    glVertex(sin(angle) * range, 0, cos(angle) * range)
+                end
             end
-            end)
+            glBeginEnd(GLTRIANGLE_FAN, drawCircle)
         end
         glBlending ("reset")
         glPopMatrix()
