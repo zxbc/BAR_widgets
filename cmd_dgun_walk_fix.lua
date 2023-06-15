73d8c8965424c71c88e1d2311967f04ad71537c2
Errrrrrr <zxbc2017@gmail.com>
2023-06-15 21:24:14 +0200

    First upload
    This widget fixes the wandering behavior of commanders after firing a dgun. It only issues a stop command 1) when the commander has finished a dgun command and 2) the commander has no commands in queue (which already prevents the walk) and 3) it has a non-zero velocity (is moving).

---
+++ cmd_dgun_walk_fix.lua
@@ -0,0 +1,33 @@
+function widget:GetInfo()
+    return {
+        name = "Commander Dgun Walk Fix",
+        desc = "Stops commander from wandering into dgun target after firing dgun.",
+        author = "Errrrrrr",
+        date = "June 2023",
+        license = "GNU GPL, v2 or later",
+        layer = 999999 + 1,
+        handler = true,
+        enabled = true,
+    }
+end
+
+local myAllyTeam = Spring.GetMyAllyTeamID()
+local dgunWalkPrevented = 0
+
+function widget:UnitCmdDone(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOpts, cmdTag)
+    -- only if it's our unit, having done a dgun command
+    if unitTeam == myAllyTeam and cmdID == 105 then
+        local commands = Spring.GetUnitCommands(unitID, -1)
+        if #commands == 0 then
+            -- issue a stop command to stop the wandering behavior only if moving
+            local x, y, z, length = Spring.GetUnitVelocity(unitID)
+            --Spring.Echo("velocity length: ".. tostring(length))
+
+            if length > 0 then
+                Spring.GiveOrderToUnit(unitID, 0, {}, {})
+                dgunWalkPrevented = dgunWalkPrevented + 1
+                Spring.Echo("Dgun walk prevented so far: " .. tostring(dgunWalkPrevented))
+            end
+        end
+    end
+end
