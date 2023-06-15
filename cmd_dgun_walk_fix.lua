c4c36dd2413057045b7e78f172fd315da58d858a
Errrrrrr <zxbc2017@gmail.com>
2023-06-15 21:43:49 +0200

    Added manual launch from Thor to the fix

--- cmd_dgun_walk_fix.lua
+++ cmd_dgun_walk_fix.lua
@@ -15,8 +15,8 @@ local myAllyTeam = Spring.GetMyAllyTeamID()
 local dgunWalkPrevented = 0
 
 function widget:UnitCmdDone(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOpts, cmdTag)
-    -- only if it's our unit, having done a dgun command
-    if unitTeam == myAllyTeam and cmdID == 105 then
+    -- only if it's our unit, having done a dgun command (or a manual launch)
+    if (unitTeam == myAllyTeam) and (cmdID == 105 or cmdID == 32102) then
         local commands = Spring.GetUnitCommands(unitID, -1)
         if #commands == 0 then
             -- issue a stop command to stop the wandering behavior only if moving
@@ -26,7 +26,8 @@ function widget:UnitCmdDone(unitID, unitDefID, unitTeam, cmdID, cmdParams, cmdOp
             if length > 0 then
                 Spring.GiveOrderToUnit(unitID, 0, {}, {})
                 dgunWalkPrevented = dgunWalkPrevented + 1
-                Spring.Echo("Dgun walk prevented so far: " .. tostring(dgunWalkPrevented))
+                local unitName = UnitDefs[unitDefID].name
+                Spring.Echo("Dgun walk prevented on ".. tostring(unitName) .. ", total prevented so far: " .. tostring(dgunWalkPrevented))
             end
         end
     end
